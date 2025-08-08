2afbcea8e8da229e91c787b5b475a410b6233f6b
The <zawwadul@gmail.com>
2025-08-09 00:58:59 +0600

    Added gun hitbox drawing in debug mode. Fixed camera

--- sandbox.c
+++ sandbox.c
@@ -6,6 +6,7 @@
 #include "gun.h"
 
 bool debugMode = false;
+#define SMALL_PLATFORM_COUNT 4
 
 void updateEnemy(Entity *enemy, Entity *player, Rectangle *platforms, int platformCount, float chaseSpeed, float chaseThreshold);
 
@@ -14,7 +15,18 @@ void sandBox() {
     int screenHeight = GetScreenHeight();
 
     float speed = 5;
-    Rectangle floor = { 0, screenHeight - 100, screenWidth * 10, 100 };
+    
+    Rectangle floor = { 0, screenHeight - 100, screenWidth * 10, 150 };
+    float floorTop = floor.y;
+    float floorBottom = floor.y + floor.height;
+    
+    Rectangle smallPlatforms[SMALL_PLATFORM_COUNT] = {
+        { screenWidth / 3, floorTop - 190, 100, 20 },
+        { screenWidth / 2, floorTop - 290, 100, 20 },
+        { screenWidth / 4, floorTop - 340, 100, 20 },
+        { screenWidth / 1.5f, floorTop - 240, 100, 20 }
+    };
+        
 
     Entity player = {
         .hitbox = { screenWidth / 2.0f - 25, screenHeight / 2.0f - 25, 35, 60 },
@@ -31,7 +43,16 @@ void sandBox() {
     InitBullets(bullets, MAX_BULLETS);
 
     Camera2D camera = { 0 };
-    camera.target = (Vector2){ player.hitbox.x + player.hitbox.width / 2, player.hitbox.y + player.hitbox.height / 2 };
+    float focusOffsetY = 50; // shift camera to focus higher above player
+    float minCameraY = screenHeight / 2.0f - 100; // don’t go below floor view
+
+    float desiredY = player.hitbox.y + player.hitbox.height / 2 - focusOffsetY;
+    if (desiredY < minCameraY) desiredY = minCameraY;
+
+    camera.target = (Vector2){
+        player.hitbox.x + player.hitbox.width / 2,
+        desiredY
+    };
     camera.offset = (Vector2){ screenWidth / 2.0f, screenHeight / 2.0f };
     camera.zoom = 1.0f;
 
@@ -46,7 +67,11 @@ void sandBox() {
             TraceLog(LOG_INFO, "Debug mode %s", debugMode ? "enabled" : "disabled");
         }
 
-        Rectangle platforms[] = { floor };
+        Rectangle platforms[1 + SMALL_PLATFORM_COUNT];
+        platforms[0] = floor;
+        for (int i = 0; i < SMALL_PLATFORM_COUNT; i++) {
+        platforms[i + 1] = smallPlatforms[i];
+        }
 
         // Movement
         // Regular movement only if not dashing
@@ -72,7 +97,7 @@ void sandBox() {
         handleDash(&player);
         handleJump(&player);
         applyGravity(&player, 0.5f);
-        updateEntity(&player, platforms, 1);
+        updateEntity(&player, platforms, 1 + SMALL_PLATFORM_COUNT);
         UpdateBullets(bullets, MAX_BULLETS);
 
 
@@ -80,7 +105,19 @@ void sandBox() {
         updateEnemy(&enemy, &player, platforms, 1, speed - 2, 5.0f);
 
         // Update camera to follow player
-        camera.target = (Vector2){ player.hitbox.x + player.hitbox.width / 2, player.hitbox.y + player.hitbox.height / 2 };
+        // Recalculate desired camera Y
+        float desiredY = player.hitbox.y + player.hitbox.height / 2 - focusOffsetY;
+
+        // Calculate lowest camera Y allowed (so camera doesn't show below floor)
+        float cameraMinY = floorBottom - screenHeight / 2.0f;
+
+        // Clamp
+        if (desiredY > cameraMinY) desiredY = cameraMinY;
+
+        camera.target = (Vector2){
+            player.hitbox.x + player.hitbox.width / 2,
+            desiredY
+        };
 
         // Drawing
         BeginDrawing();
@@ -89,6 +126,9 @@ void sandBox() {
         BeginMode2D(camera);
 
         DrawRectangleRec(floor, BLACK);
+        for (int i = 0; i < SMALL_PLATFORM_COUNT; i++) {
+        DrawRectangleRec(smallPlatforms[i], PURPLE);
+        }
         DrawRectangleRec(player.hitbox, RAYWHITE);
         DrawRectangleRec(enemy.hitbox, RED);
         DrawBullets(bullets, MAX_BULLETS);
@@ -98,6 +138,14 @@ void sandBox() {
             DrawRectangleLines(player.hitbox.x, player.hitbox.y, player.hitbox.width, player.hitbox.height, BLUE);
             DrawRectangleLines(floor.x, floor.y, floor.width, floor.height, GREEN);
             DrawRectangleLines(enemy.hitbox.x, enemy.hitbox.y, enemy.hitbox.width, enemy.hitbox.height, ORANGE);
+            for (int i = 0; i < SMALL_PLATFORM_COUNT; i++) {
+            DrawRectangleLines(smallPlatforms[i].x, smallPlatforms[i].y, smallPlatforms[i].width, smallPlatforms[i].height, GREEN);
+            }
+            for (int i = 0; i < MAX_BULLETS; i++) {
+            if (bullets[i].active) {
+                DrawRectangleLinesEx(bullets[i].hitbox, 2, PURPLE); 
+            }
+        }
         }
 
         EndMode2D();
