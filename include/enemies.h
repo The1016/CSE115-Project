315379eaa1f32893659baab539e8beabfaafc5de
The <zawwadul@gmail.com>
2025-08-20 02:43:09 +0600

    Added bossfight, poggoing, enemy hit recoil

--- include/enemies.h
+++ include/enemies.h
@@ -13,7 +13,19 @@ typedef struct{
     int patrolDistance; // Distance to patrol before changing direction
 }chaser;
 
+typedef struct {
+    Entity base; // Inherit from Entity
+    pos spawnPos; // Spawn position of the boss
+    float attackCooldown; // Time until next attack
+    float attackWindup; // Time spent charging the attack
+    bool isCharging;   
+    float deathTimer; // Timer for death animation
+    float health; // Boss health
+    bool isAwake;
+    bool showName; 
+    float nameTimer;
+} boss1;
 
 void Chasers(chaser *enemy, Player *player, Rectangle *platforms, int platformCount);
-
+void Boss1(boss1 *enemy, Player *player, Rectangle *platforms, int platformCount);
 #endif
